c8542c06be1f984cf2467154784b6ef3d177a7a9
Tarun Telang <tarun.telang@gmail.com>
2023-11-29 21:35:17 +0530

    Moving files inside multiplication folder
    Moving files inside multiplication folder for better organization of sources.

--- src/main/java/microservices/book/multiplication/challenge/Challenge.java
+++
@@ -1,7 +0,0 @@
-package microservices.book.multiplication.challenge;
-
-/**
- * This class represents a Challenge to solve a Multiplication (a * b).
- */
-public record Challenge(int factorA, int factorB) {
-}
--- src/main/java/microservices/book/multiplication/user/User.java
+++
@@ -1,8 +0,0 @@
-package microservices.book.multiplication.user;
-
-/**
- * Stores information to identify the user.
- */
-public record User(Long id, String alias) {
-}
-
